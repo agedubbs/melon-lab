ce106f469bb668b61edeb0feb352206801504dcc
Matthias Gemperli <hello@matthiasgemperli.ch>
2018-06-21 10:17:05 +0200

    Change header styles

--- packages/manager-components/src/components/Header/index.tsx
+++ packages/manager-components/src/components/Header/index.tsx
@@ -2,9 +2,11 @@ import React, { StatelessComponent } from 'react';
 import Icon from '~/blocks/Icon';
 import styles from './styles.css';
 
-export interface HeaderProps {}
+export interface HeaderProps {
+  info?: string;
+}
 
-export const Header: StatelessComponent<HeaderProps> = ({ children }) => {
+export const Header: StatelessComponent<HeaderProps> = ({ children, info }) => {
   return (
     <div className="header">
       <style jsx>{styles}</style>
@@ -13,29 +15,30 @@ export const Header: StatelessComponent<HeaderProps> = ({ children }) => {
         <Icon width="25px" height="25px" name="logos_default" />
       </div>
       <div className="header__name">Peter-Fund</div>
-      <div className="header__price">
-        <span className="header__price-desc">Share price:</span>
-        <span className="header__price-value">1.000 MLN-T-M/Share</span>
-      </div>
-      <div className="header__price">
-        <span className="header__price-desc">AUM:</span>
-        <span className="header__price-value">1.000 MLN-T-M/Share</span>
-      </div>
-      <div className="header__price">
-        <span className="header__price-desc">Ranking:</span>
-        <span className="header__price-value">
-          <span className="header__price-value-important">188</span> out of 287 Melon
-          Funds
-        </span>
-      </div>
       <div className="header__account">
         <span className="header__account-address">0xA5f0…2783</span>
         <span className="header__account-balances">
           <span className="header__account-balance">Ⓜ 8.0000</span>
           <span className="header__account-balance">Ξ 0.3543</span>
         </span>
-        <span className="header__account-net">kovan</span>
-        <span className="header__account-info">Price feed down</span>
+        {info && <span className="header__account-info">Price feed down</span>}
+      </div>
+      <div className="header__fund-info">
+        <div className="header__price">
+          <span className="header__price-desc">Share price:</span>
+          <span className="header__price-value">1.000 MLN-T-M/Share</span>
+        </div>
+        <div className="header__price">
+          <span className="header__price-desc">AUM:</span>
+          <span className="header__price-value">1.000 MLN-T-M/Share</span>
+        </div>
+        <div className="header__price">
+          <span className="header__price-desc">Ranking:</span>
+          <span className="header__price-value">
+            <span className="header__price-value-important">188</span> out of 287
+            Melon Funds
+          </span>
+        </div>
       </div>
     </div>
   );
